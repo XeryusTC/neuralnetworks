f1a9bac9add7e42b395ecccce9b23f9b27505e70
XeryusTC <armadillo@onenetbeyond.org>
2016-12-03 21:34:37 +0100

    Add Rosenblat training algorithm

--- rosenblat.py
+++ rosenblat.py
@@ -10,5 +10,23 @@ def generate_data(P, N):
 
     return data, labels
 
+def train_rosenblat(data, labels, n_max):
+    N = data.shape[1]
+    w = np.zeros(N)
+    for n in range(n_max):
+        print('Starting training epoch', n)
+        success = True
+        for t in range(labels.size):
+            E = w.dot(data[t]) * labels[t]
+            if E <= 0:
+                success = False
+                w = w + (1/N) * data[t] * labels[t]
+        # Stop training if all E > 0
+        if success:
+            break
+    return w
+
 if __name__ == '__main__':
-    print(generate_data(10, 20))
+    data, labels = generate_data(10, 20)
+
+    train_rosenblat(data, labels, 5)
