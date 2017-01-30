6f72d49306717802723abd5aff602c1c63ba5de6
XeryusTC <armadillo@onenetbeyond.org>
2017-01-31 00:32:00 +0100

    Final version of report 2

--- minover.py
+++ minover.py
@@ -49,7 +49,26 @@ def train_minover(data, labels, R, n_max):
     print(error)
     return t, error
 
+def train_rosenblat(data, labels, R, n_max):
+    N = data.shape[1]
+    P = labels.size
+    w = np.zeros(N)
+    for n in range(n_max):
+        success = True
+        for t in range(P):
+            E = w.dot(data[t]) * labels[t]
+            if E <= 0:
+                success = False
+                w = w + (1/N) * data[t] * labels[t]
+        if success:
+            break
+    error = (1/np.pi) * np.arccos(w.dot(R.wstar) / (np.linalg.norm(w) * \
+            np.linalg.norm(R.wstar)))
+    print(error)
+    return n*P + t, error
+
 def experiment(N, alphas, n_D, n_max):
+    print("Running minover experiment")
     with open('minover_results.csv', 'w') as f:
         f.write('alpha,n,t,err\n')
         for a in alphas:
@@ -61,8 +80,22 @@ def experiment(N, alphas, n_D, n_max):
                 f.write('{},{},{},{}\n'.format(a, n, t, err))
             f.flush()
 
+def experiment_rosenblat(N, alphas, n_D, n_max):
+    print("Running Rosenblat experiment")
+    with open('rosenblat_e_g_results.csv', 'w') as f:
+        f.write('alpha,n,t,err\n')
+        for a in alphas:
+            P = int(N*a)
+            print('Running Rosenblat experiment for P={}'.format(P))
+            for n in range(n_D):
+                data, labels, R = generate_data(P, N)
+                t, err = train_rosenblat(data, labels, R, n_max)
+                f.write('{},{},{},{}\n'.format(a, n, t, err))
+            f.flush()
+
 if __name__ == '__main__':
     #alphas = np.linspace(0.5, 5.0, 20)
     alphas = np.arange(0.05, 9.1, 0.1)
     print(alphas)
-    experiment(20, alphas, 20, 25000)
+    experiment_rosenblat(20, alphas, 20, 1000)
+    #experiment(20, alphas, 20, 25000)
